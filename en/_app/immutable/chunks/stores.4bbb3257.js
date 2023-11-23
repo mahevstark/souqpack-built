a8d42c4627a294acdb41174fe74f81b3abfb0f15
Mahev Stark <mahevstark@gmail.com>
2023-11-23 16:21:02 +0500

    build test  3

--- en/_app/immutable/chunks/stores.4bbb3257.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./singletons.f83e7e9a.js";const a=t({show:!1,message:"",type:""}),e=t({token:"",user:{},isLoggedIn:!1}),o=t({items:[],total:0,totalItems:0});export{e as a,o as c,a as t};
---
+++ en/en/_app/immutable/chunks/stores.5fd4483f.js
@@ -0,0 +1,1 @@
+import{w as t}from"./singletons.70f87a2c.js";const a=t({show:!1,message:"",type:""}),e=t({token:"",user:{},isLoggedIn:!1}),o=t({items:[],total:0,totalItems:0});export{e as a,o as c,a as t};
